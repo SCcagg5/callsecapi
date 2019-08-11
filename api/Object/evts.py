749ae6580bb819d413eeea7c9b991c28d78c7d84
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2019-08-11 23:55:52 +0200

    adding src

--- api/Object/evts.py
+++ api/Object/evts.py
@@ -1,27 +1,45 @@
 import requests
+import os
+import time
 
-class evts:
-    def check_method(method):
-        auth = ["put", "delete", "patch", "post", "head", "options", "get"]
-        for i in auth:
-            if i == str(method):
-                return [True, None, None]
-        return [False, "invalid method " + str(method), 401]
+docker = "/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker.exe"
+dc = "/mnt/c/Program\ Files/Docker/Docker/resources/bin/docker-compose.exe"#"docker-compose"
 
-    def call(url, method, data = None):
-        auth={  "put" : [requests.put, (url)],
-                "delete": [requests.delete, (url)],
-                "patch": [requests.patch, (url)],
-                "post": [requests.post, (url, {"data": data})],
-                "head": [requests.head, (url)],
-                "options": [requests.options, (url)],
-                "get": [requests.get, {"params": data}]
-        }
-        try:
-            r = auth[method][0](url, **auth[method][1])
-        except:
-            return [False, "Invalid url" , 401]
-        return [True, {"data": r.text, "status": r.status_code}, 200]
+class dockers:
+
+    def run(x, login):
+        name = "wordpress_n" + str(x)
+        commands = [
+        "cd ../..",
+        "cp -r WP_base/ " + name,
+        "cd " + name,
+        dc + " up -d > /dev/null",
+        "echo $(" + dc + " ps nginx | tail -n 1 | cut -d : -f2 | cut -d - -f1) > port.conf"
+        ]
+        globalcom = ""
+        for i in commands:
+            globalcom += str(i) + " ; "
+        os.system(globalcom)
+        with open("../../" + name + "/port.conf", 'r') as file:
+            data = file.read()[:-1]
+            file.close()
+        return dockers.nginxsteup(data, login, x)
+
+    def nginxsteup(port, login, x):
+        with open("../../nginx/site.conf", 'r') as file:
+            conf = file.read()[:-2]
+            file.close()
+        url = "/" + login + "/" + str(x) + "/"
+        conf += "\n\
+    location /courte_e/1/ {\n\
+        proxy_pass http://host.docker.internal:"+port+"/ ;\n\
+    }\
+}"
+        with open("../../nginx/site.conf", 'w') as file:
+            file.write(conf)
+            file.close()
+        os.system("cd ../../ ;" + dc + " restart")
+        return [True, {"url": url, "addr": port}]
 
 if __name__ == '__main__':
-    print(evts.call("http://google.com", "get")[1])
+    print(dockers.run(1, "courte_e")[1])
